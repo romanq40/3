5e92cccb33e0dca04cd9cb81b60af3c69115ea92
romanq40 <romanq40@mail.ru>
2024-08-22 20:17:27 +0500

    first commit

---
+++ homework3.py
@@ -0,0 +1,37 @@
+
+
+# def uname():
+#     name = input('Как тебя зовут')
+#     last_name = input ('Какая у тебя фамилия ')
+#     print(f'Привет {name} {last_name}')
+
+# uname()
+
+# def number():
+#     a = int(input('Введи первое число'))
+#     b = int(input('Введи второе число '))
+#     summ = 0 
+#     for i in range(a,b):
+#         if i % 2 == 0:
+#             summ +=i
+#     print(summ)
+
+# number()
+
+# l = [321,421,5,2354,1]
+
+# def max(number):
+#     i = 0
+#     for m in number:
+#         if i < m:
+#             i = m
+#     return i
+# print(max(l))
+
+def num():
+    a = input('Введите число')
+    print(len(a))
+
+num()
+
+    
